19f35ba9dd7bb51e425ced9d833099e01eae50d5
fstipo <fstipanov@gmail.com>
2022-09-30 17:55:56 +0200

    use react moment for Dates

--- src/components/ExpensesItem.js
+++ src/components/ExpensesItem.js
@@ -1,12 +1,17 @@
 import React from 'react';
+import Moment from 'moment';
 import './ExpensesItem.css';
 
 const ExpensesItem = () => {
+  const expenseDate = new Date(2022, 10, 30).toISOString();
+  const expenseTitle = 'New Desk';
+  const expensePrice = 514.24;
+
   return (
     <div className="expense-item">
-      <div>March 28th 2022</div>
-      <h2 className="expense-item__description">Car Insurance</h2>
-      <div className="expense-item__price">$224,78</div>
+      <div>{Moment(expenseDate).format('MMMM Do YYYY')}</div>
+      <h2 className="expense-item__description">{expenseTitle}</h2>
+      <div className="expense-item__price">${expensePrice}</div>
     </div>
   );
 };
