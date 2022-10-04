49e0e27991fa81f2f84bd2b46465dddceb9f0f95
fstipo <fstipanov@gmail.com>
2022-10-04 11:35:26 +0200

    add props to expenseItem

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 import ExpensesItem from "./components/ExpensesItem";
+const expenses = [
+  { id: "e1", title: "Car Insurance", amount: 94.12, date: new Date(2320, 7, 12) },
+  { id: "e2", title: "New Desk", amount: 1004.56, date: new Date(2021, 21, 8) },
+  { id: "e3", title: "TV", amount: 991.15, date: new Date(2019, 5, 10) },
+  { id: "e4", title: "Toilet paper", amount: 14.12, date: new Date(2021, 8, 2) },
+  { id: "e5", title: "Bike", amount: 450, date: new Date(20202, 17, 11) },
+]
 
 function App() {
   return (
     <div>
       <h1>Let's get started</h1>
-      <ExpensesItem />
+      <ExpensesItem title={expenses[0].title} price={expenses[0].amount} date={expenses[0].date} />
+      <ExpensesItem title={expenses[1].title} price={expenses[1].amount} date={expenses[1].date} />
+      <ExpensesItem title={expenses[3].title} price={expenses[3].amount} date={expenses[2].date} />
+      <ExpensesItem title={expenses[2].title} price={expenses[2].amount} date={expenses[3].date} />
     </div>
-  );
+  )
 }
+
+
+
 
 export default App;
--- src/components/ExpensesItem.js
+++ src/components/ExpensesItem.js
@@ -2,16 +2,15 @@ import React from 'react';
 import Moment from 'moment';
 import './ExpensesItem.css';
 
-const ExpensesItem = () => {
-  const expenseDate = new Date(2022, 10, 30).toISOString();
-  const expenseTitle = 'New Desk';
-  const expensePrice = 514.24;
+const ExpensesItem = (props) => {
+  const expenseDate = props.date.toISOString();
 
   return (
     <div className="expense-item">
+      {/* <div>{Moment(expenseDate).format('MMMM Do YYYY')}</div> */}
       <div>{Moment(expenseDate).format('MMMM Do YYYY')}</div>
-      <h2 className="expense-item__description">{expenseTitle}</h2>
-      <div className="expense-item__price">${expensePrice}</div>
+      <h2 className="expense-item__description">{props.title}</h2>
+      <div className="expense-item__price">${props.price}</div>
     </div>
   );
 };
