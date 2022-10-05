3f5a51e957a534e90e8cdbe63c5b25c5f6052c7e
fstipo <fstipanov@gmail.com>
2022-10-05 11:29:48 +0200

    folder structure

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React from "react";
-import { expensesList as expenses } from "./components/utils";
-import Expenses from "./components/Expenses";
-import Card from "./components/Card";
-import "./components/Expenses.css"
+import { expensesList as expensesListData } from "./components/Utils/utils";
+import Expenses from "./components/Expenses/Expenses";
+import Card from "./components/UI/Card";
+import "./components/Expenses/Expenses.css"
 
 const App = () => {
+  // JSX
+  // return (
+  //   React.createElement(Card, { className: "expenses" }, React.createElement(Expenses, { expenseData: expensesListData }))
+  // )
   return (
     <Card className="expenses">
-      <Expenses expenseData={expenses} />
+      <Expenses expenseData={expensesListData} />
     </Card>
   )
 }
